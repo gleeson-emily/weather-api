fe318fd3d52551604119c8b36ff52a29abad02c6
Emily Gleeson <engleeson@gmail.com>
2021-06-08 20:45:12 -0400

    pulled data for current forecast

--- logic.js
+++ logic.js
@@ -35,24 +35,47 @@ searchButton.addEventListener("click", (event) => {
       })
       .then(function (data) {
         console.log(data);
-        console.log(data["daily"][1].weather)
-        console.log(data["daily"][0].weather[0].description)
+        console.log("this is daily weather", data.daily[1].weather[0].description)
+        console.log(data["daily"][0].weather[0])
         console.log(data["daily"][1].temp.day)
         console.log(data["daily"][0].wind_speed)
         console.log(data["daily"][0].humidity)
+        console.log(data.current.temp)
+        console.log(data.current.humidity)
+        console.log(data.current.weather[0].description)
+        console.log(data.current.uvi)
+        console.log(data.current.wind_speed)
       
-    var mainForecast = document.getElementById("forecast-main");
+        //current weather
+      var mainForecast = document.getElementById("forecast-main");
+      var forecastCard = document.createElement("div");
+      forecastCard.classList.add("card");
+      mainForecast.appendChild(forecastCard);
+      var currentIconCall = data.current.weather[0].icon;
+      var currentIcon = document.createElement("img");
+      currentIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + currentIconCall + "@2x.png");
+      currentIcon.setAttribute("height", "100px");
+      currentIcon.setAttribute("width", "100px");
+      forecastCard.appendChild(currentIcon);
+      var date = moment().format("dddd MMM Do");
+      var currentWeather = document.createElement("p");
+      forecastCard.appendChild(currentWeather);
+      var currentTemperature = data.current.temp;
+      var currentWind = data.current.wind_speed;
+      var currentConditions = data.current.weather[0].description;
+      var currentHumidity = data.current.humidity;
+      var currentUvi = data.current.uvi;
+      currentWeather.textContent = currentConditions + ", " + currentTemperature + "°C, Humidity: " + currentHumidity + "%, Wind Speed: " + currentWind + "km/h, UV Index: " + currentUvi;
+      
+      //5-day forecast
         for (i=0; i<5; i++) {
-          var forecastCard = document.createElement("div");
           var weatherInfo = document.createElement("p");
           var temperatureList = document.createElement("p");
-          forecastCard.classList.add("card");
-          mainForecast.appendChild(forecastCard);
           var iconCall = data["daily"][i].weather[0].icon;
           var weatherIcon = document.createElement("img");
           weatherIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + iconCall + "@2x.png");
-          weatherIcon.setAttribute("height", "75px");
-          weatherIcon.setAttribute("width", "75px");
+          weatherIcon.setAttribute("height", "50px");
+          weatherIcon.setAttribute("width", "50px");
           var weatherDescription = data["daily"][i].weather[0].description;
           var uvi = data["daily"][i].uvi;
           var dailyTemperature = data["daily"][i].temp.day;
